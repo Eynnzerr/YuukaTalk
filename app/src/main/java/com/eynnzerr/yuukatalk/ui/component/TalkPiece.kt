da5dbac91b3cdac4c5f69c00e59d7a69267adb19
eynnzerr <809204291@qq.com>
2023-11-29 23:19:08 +0800

    feat: implement chat bubble.

--- app/src/main/java/com/eynnzerr/yuukatalk/ui/component/TalkPiece.kt
+++ app/src/main/java/com/eynnzerr/yuukatalk/ui/component/TalkPiece.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.eynnzerr.yuukatalk.data.model.Character
 import com.eynnzerr.yuukatalk.data.model.Talk
+import com.eynnzerr.yuukatalk.ui.theme.YuukaTalkTheme
 
 @Composable
 fun TalkPiece(talkData: Talk) {
@@ -23,19 +26,30 @@ fun TalkPiece(talkData: Talk) {
 @Composable
 private fun PureTextPiece(talk: Talk.PureText) {
     if (talk.isFirst) {
-        Row {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             StudentAvatar(
-                url = talk.talker.avatarPath
+                url = talk.talker.avatarPath,
+                size = 64.dp
             )
             Column(
                 modifier = Modifier.padding(start = 8.dp)
             ) {
-                Text(text = talk.talker.name)
-                Text(text = talk.text)
+                Text(
+                    modifier = Modifier.padding(start = 8.dp),
+                    text = talk.talker.name,
+                    fontSize = 18.sp
+                )
+                ChatBubble(
+                    text = talk.text,
+                    isMyMessage = false,
+                    showArrow = true
+                )
             }
         }
     } else {
-        Text(text = talk.text)
+        ChatBubble(text = talk.text, isMyMessage = false, showArrow = false)
     }
 }
 
@@ -55,17 +69,19 @@ private fun NarrationPiece(talk: Talk.Narration) {
 )
 @Composable
 fun FirstPureTextPiecePreview() {
-    val character = Character(
-        name = "Shiroko",
-        school = "Abydios",
-        avatarPath = "file:///android_asset/shiroko/emoji_0.png",
-    )
-    val talk = Talk.PureText(
-        talker = character,
-        isFirst = true,
-        text = "Hello, sensei."
-    )
-    PureTextPiece(talk = talk)
+    YuukaTalkTheme {
+        val character = Character(
+            name = "Shiroko",
+            school = "Abydios",
+            avatarPath = "file:///android_asset/shiroko/emoji_0.png",
+        )
+        val talk = Talk.PureText(
+            talker = character,
+            isFirst = true,
+            text = "Hello, sensei."
+        )
+        PureTextPiece(talk = talk)
+    }
 }
 
 @Preview(
@@ -74,15 +90,17 @@ fun FirstPureTextPiecePreview() {
 )
 @Composable
 fun FollowingPureTextPiecePreview() {
-    val character = Character(
-        name = "Shiroko",
-        school = "Abydios",
-        avatarPath = "file:///android_asset/shiroko/emoji_0.png",
-    )
-    val talk = Talk.PureText(
-        talker = character,
-        isFirst = false,
-        text = "Don't let me go."
-    )
-    PureTextPiece(talk = talk)
+    YuukaTalkTheme {
+        val character = Character(
+            name = "Shiroko",
+            school = "Abydios",
+            avatarPath = "file:///android_asset/shiroko/emoji_0.png",
+        )
+        val talk = Talk.PureText(
+            talker = character,
+            isFirst = false,
+            text = "Don't let me go."
+        )
+        PureTextPiece(talk = talk)
+    }
 }
